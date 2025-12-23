7c61b99541a5c77c79c9dc0f29d98b2ea006308f
NilYldz <Ntugceyildiz@gmail.com>
2025-12-23 22:23:43 +0300

    m4

--- gts/views.py
+++ gts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Thesis
 
-# Create your views here.
+def thesis_list(request):
+    theses = Thesis.objects.all()
+    context = {
+        'theses': theses
+    }
+    return render(request, 'thesis_list.html', context)
