c06d0fa9c892c865ec0b1e80f4ecebc82ee6588c
NilYldz <Ntugceyildiz@gmail.com>
2025-12-23 21:51:30 +0300

    m2

--- gts/models.py
+++ gts/models.py
@@ -1,82 +1,71 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
-
-class Thesis(models.Model):
-    thesis_no = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=500)
-    abstract = models.TextField()
-    author = models.ForeignKey(
-        'Author',
-        on_delete=models.CASCADE,
-        db_column='author_id'
-    )
-    year = models.IntegerField()
-    type = models.ForeignKey(
-        'ThesisType',
-        on_delete=models.CASCADE,
-        db_column='type_id'
-    )
-    institute = models.ForeignKey(
-        'Institute',
-        on_delete=models.CASCADE,
-        db_column='institute_id'
-    )
-    num_pages = models.IntegerField()
-    language = models.ForeignKey(
-        'Language',
-        on_delete=models.CASCADE,
-        db_column='language_id'
-    )
-    submission_date = models.DateField()
-
-    class Meta:
-        managed = False
-        db_table = 'thesis'
-
-    def __str__(self):
-        return f"{self.thesis_no} - {self.title}"
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
+   
+    name = models.CharField(max_length=255, verbose_name="Yazar Adı Soyadı")
 
     class Meta:
-        managed = False
         db_table = 'author'
+       
 
     def __str__(self):
-        return str(self.author_id)
+        return self.name  
+
 
 
 class ThesisType(models.Model):
     type_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, verbose_name="Tez Türü")
 
     class Meta:
-        managed = False
         db_table = 'thesis_type'
 
     def __str__(self):
-        return str(self.type_id)
+        return self.name
 
 
 class Institute(models.Model):
     institute_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name="Üniversite/Enstitü Adı")
 
     class Meta:
-        managed = False
         db_table = 'institute'
 
     def __str__(self):
-        return str(self.institute_id)
+        return self.name
+
 
 
 class Language(models.Model):
     language_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, verbose_name="Dil")
 
     class Meta:
-        managed = False
         db_table = 'language'
 
     def __str__(self):
-        return str(self.language_id)
+        return self.name
+
+
+
+class Thesis(models.Model):
+    thesis_no = models.IntegerField(primary_key=True, verbose_name="Tez Numarası")
+    title = models.CharField(max_length=500, verbose_name="Başlık")
+    abstract = models.TextField(verbose_name="Özet")
+    year = models.IntegerField(verbose_name="Yıl")
+    num_pages = models.IntegerField(verbose_name="Sayfa Sayısı")
+    submission_date = models.DateField(verbose_name="Teslim Tarihi")
+
+    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, db_column='author_id', verbose_name="Yazar")
+    type = models.ForeignKey(ThesisType, on_delete=models.CASCADE, db_column='type_id', verbose_name="Tez Türü")
+    institute = models.ForeignKey(Institute, on_delete=models.CASCADE, db_column='institute_id', verbose_name="Üniversite")
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, db_column='language_id', verbose_name="Dil")
+
+    class Meta:
+        db_table = 'thesis'
+
+    def __str__(self):
+        return f"{self.thesis_no} - {self.title}"
