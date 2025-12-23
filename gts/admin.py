31dad293b0635b1daa5b991129055318f81007c2
NilYldz <Ntugceyildiz@gmail.com>
2025-12-23 21:26:22 +0300

    m

---
+++ gts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Thesis, Author, ThesisType, Institute, Language
+
+
+admin.site.register(Thesis)
+admin.site.register(Author)
+admin.site.register(ThesisType)
+admin.site.register(Institute)
+admin.site.register(Language)
