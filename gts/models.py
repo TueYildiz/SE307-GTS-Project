21e4651d8f74733bbe74b5191434ca6dc0e01042
Yigit Ersoy <141333777+yigitersoyy@users.noreply.github.com>
2025-12-27 22:21:32 +0300

    Add University, Institute, Supervisor, and ThesisType models

--- gts/models.py
+++ gts/models.py
@@ -1,18 +1,51 @@
 from django.db import models
 
 
+class University(models.Model):
+    university_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name="Üniversite Adı")
+
+    class Meta:
+        db_table = 'university'
+        verbose_name_plural = "Universities"
+
+    def __str__(self):
+        return self.name
+
+
+class Institute(models.Model):
+    institute_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name="Enstitü Adı")
+   
+    university = models.ForeignKey(University, on_delete=models.CASCADE, db_column='university_id', verbose_name="Bağlı Olduğu Üniversite")
+
+    class Meta:
+        db_table = 'institute'
+
+    def __str__(self):
+        return f"{self.name} ({self.university.name})"
+
+# --- MEVCUT: Yazar Modeli ---
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
-   
     name = models.CharField(max_length=255, verbose_name="Yazar Adı Soyadı")
 
     class Meta:
         db_table = 'author'
-       
 
     def __str__(self):
         return self.name  
 
+
+class Supervisor(models.Model):
+    supervisor_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name="Danışman Adı Soyadı")
+
+    class Meta:
+        db_table = 'supervisor'
+
+    def __str__(self):
+        return self.name
 
 
 class ThesisType(models.Model):
@@ -26,18 +59,6 @@ class ThesisType(models.Model):
         return self.name
 
 
-class Institute(models.Model):
-    institute_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, verbose_name="Üniversite/Enstitü Adı")
-
-    class Meta:
-        db_table = 'institute'
-
-    def __str__(self):
-        return self.name
-
-
-
 class Language(models.Model):
     language_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, verbose_name="Dil")
@@ -48,6 +69,26 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+class SubjectTopic(models.Model):
+    topic_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name="Konu Başlığı")
+
+    class Meta:
+        db_table = 'subject_topic'
+
+    def __str__(self):
+        return self.name
+
+
+class Keyword(models.Model):
+    keyword_id = models.AutoField(primary_key=True)
+    word = models.CharField(max_length=100, verbose_name="Anahtar Kelime")
+
+    class Meta:
+        db_table = 'keyword'
+
+    def __str__(self):
+        return self.word
 
 
 class Thesis(models.Model):
@@ -61,11 +102,19 @@ class Thesis(models.Model):
     
     author = models.ForeignKey(Author, on_delete=models.CASCADE, db_column='author_id', verbose_name="Yazar")
     type = models.ForeignKey(ThesisType, on_delete=models.CASCADE, db_column='type_id', verbose_name="Tez Türü")
-    institute = models.ForeignKey(Institute, on_delete=models.CASCADE, db_column='institute_id', verbose_name="Üniversite")
+    institute = models.ForeignKey(Institute, on_delete=models.CASCADE, db_column='institute_id', verbose_name="Enstitü")
     language = models.ForeignKey(Language, on_delete=models.CASCADE, db_column='language_id', verbose_name="Dil")
+    
+    
+    supervisor = models.ForeignKey(Supervisor, on_delete=models.CASCADE, related_name='supervised_theses', verbose_name="Danışman")
+    co_supervisor = models.ForeignKey(Supervisor, on_delete=models.SET_NULL, null=True, blank=True, related_name='co_supervised_theses', verbose_name="Eş Danışman")
+    
+    topics = models.ManyToManyField(SubjectTopic, verbose_name="Konu Başlıkları")
+    keywords = models.ManyToManyField(Keyword, blank=True, verbose_name="Anahtar Kelimeler")
 
     class Meta:
         db_table = 'thesis'
+        verbose_name_plural = "Theses"
 
     def __str__(self):
         return f"{self.thesis_no} - {self.title}"
