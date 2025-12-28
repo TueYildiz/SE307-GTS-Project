1405b574d36e00695a0f96654d6ce24ac2d396df
NilYldz <Ntugceyildiz@gmail.com>
2025-12-28 22:22:27 +0300

    settingspyupdate

--- gts/settings.py
+++ gts/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gts_db',              # Veritabanı adımız
+        'USER': 'root',                # MySQL kullanıcı adı (genelde root)
+        'PASSWORD': '',  # <--- DİKKAT!
+        'HOST': '127.0.0.1',           # Localhost
+        'PORT': '3306',                # Standart port
     }
 }
 
